2d875916c29ca5a40474a030368b4c99757fa5b6
Mischka Booley <mischka@gearbox.com.au>
2022-12-03 23:49:04 +1100

    add stay logged in function to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,19 @@ class UsersController < ApplicationController
       render json: { error: "Invalid request" }, status: :bad_request
     end  
   end
+  
+  # stay logged in
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
 
   private
   def user_params
     params.require(:user).permit(:username, :password, :email)
   end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/me', to: 'users#show'
   resources :users, only: [:create]
 
 
