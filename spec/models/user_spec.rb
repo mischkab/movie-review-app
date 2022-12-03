082c5049a9feee07f8e514faae3042dd3bcc469c
Mischka Booley <mischka@gearbox.com.au>
2022-12-03 18:58:42 +1100

    added user validations for model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'user authentication validations' do
+    it 'ensures that a new user has a name' do
+      #arrange
+      user = User.new(username: "")
+
+      #act
+      user.save
+
+      #assert
+      expect(user.valid?).to be false
+    end
+
+    it { should have_secure_password }
+
+    it { should validate_uniqueness_of(:username) }
+    
+    it { should validate_uniqueness_of(:email) }
+
+  end
 end
