862df781c8a6911dd5978bb561a83b5a30451fc0
Mischka Booley <mischka@gearbox.com.au>
2022-12-03 21:05:18 +1100

    add users#create and test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: user, status: :created
+    else 
+      render json: { error: "Invalid request" }, status: :bad_request
+    end  
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  # describe "GET /index" do
+  #   pending "add some examples (or delete) #{__FILE__}"
+  # end
+
+  describe "POST /users" do
+    it "creates a user with valid parameters" do
+      #arrange
+      body = { 
+        user: {
+          username: "user_one",
+          password: "password1",
+          email: "email@test.com"
+        }
+      }
+      
+      #act
+      post "/users", params: body
+
+      #assert
+      expect(response).to have_http_status(:created)
+      expect(response.body).to include("user_one")
+    end
   end
 end
