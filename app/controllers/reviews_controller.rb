42ba051fb5bcfd8707fba8a5f68274ecdb14abde
Mischka Booley <mischka@gearbox.com.au>
2022-12-04 23:00:12 +1100

    attempt to add review create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  before_action :find_user
+  
+  def create
+    @review = Review.create!(review_params)
+    @review.user_id = current_user.id
+    render json: review.as_json(except: [:created_at, :updated_at])
+  end
+
+  private
+
+  def find_user
+    @current_user = User.find(2)
+  end
+
+  def review_params
+    params.permit(:rating, :comment, :movie_id)
+  end
 end
