925af7162f234276150f37e489c6b7e48f037c26
Mischka Booley <mischka@gearbox.com.au>
2022-12-04 22:36:39 +1100

    add many-to-many relationship

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  has_many :users, through: :reviews
 
   validates :title, :director, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :reviews
+  has_many :movies, through: :reviews
 
   has_secure_password
   validates :username, presence: true, uniqueness: true
